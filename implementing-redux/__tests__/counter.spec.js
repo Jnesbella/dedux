204abc5b166ea9a340bff8e4beef939bc42c4d5f
Jesse Nesbella <jnesbella@me.com>
2021-08-19 11:35:24 -0400

    Add counter tests

---
+++ implementing-redux/__tests__/counter.spec.js
@@ -0,0 +1,52 @@
+import Dedux from '../dedux';
+import {
+  counterReducer,
+  incrementCounter,
+  decrementCounter,
+  resetCounter,
+  INITIAL_STATE,
+} from '../counter';
+
+const { createStore } = Dedux;
+
+/*======================================================
+                          TESTS
+======================================================*/
+
+const sut = () => createStore(counterReducer);
+
+describe.skip('counter', () => {
+  it('initializes to the initial state', () => {
+    const store = sut();
+
+    expect(store.getState()).toBe(INITIAL_STATE);
+  });
+
+  describe('incrementCounter', () => {
+    it('increments the state', () => {
+      const store = sut();
+      store.dispatch(incrementCounter());
+
+      expect(store.getState()).toBe(1);
+    });
+  });
+
+  describe('decrementCounter', () => {
+    it('decrements the state', () => {
+      const store = sut();
+      store.dispatch(decrementCounter());
+
+      expect(store.getState()).toBe(-1);
+    });
+  });
+
+  describe('resetCounter', () => {
+    it('resets the state', () => {
+      const store = sut();
+      store.dispatch(incrementCounter());
+      store.dispatch(resetCounter());
+
+      expect(store.getState()).toBe(0);
+    });
+  });
+});
